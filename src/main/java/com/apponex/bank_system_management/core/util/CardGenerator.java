8759642af94ad337c21fcc511a80452740a9da74
BayarovEmil <149266020+BayarovEmil@users.noreply.github.com>
2024-08-12 17:49:25 +0400

    Account management features

---
+++ src/main/java/com/apponex/bank_system_management/core/util/CardGenerator.java
@@ -0,0 +1,2 @@
+package com.apponex.bank_system_management.core.util;public class CardGenerator {
+}
---
+++ src/main/java/com/apponex/bank_system_management/dto/account/CardResponse.java
@@ -0,0 +1,2 @@
+package com.apponex.bank_system_management.dto.account;public class CardResponse {
+}
